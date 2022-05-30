55433d0d1e2bd75953095e9e082ad31a9a96cc12
Minquan Tan <minch_tan@hotmail.com>
2022-05-30 14:00:57 +0800

    provided root for index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,6 +9,22 @@ class ListsController < ApplicationController
     # raise
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to @list
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   def new
     @list = List.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: 'articles#index'
+  root to: 'lists#index'
 
-  resources :lists, only: %i[index show new edit create] do
+  resources :lists, execpt: %i[index] do
     resources :bookmarks, only: %i[index new create]
   end
   resources :bookmarks, only: %i[destroy]
